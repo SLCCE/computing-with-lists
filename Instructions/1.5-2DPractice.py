9b00414c337913bfb2d364c49ed9dbc404e2670d
Jason Yin <jyin23@ncsu.edu>
2026-02-02 19:40:10 -0500

    remaining material for lists
    Co-authored-by: CarlosPoteet <CarlosPoteet@users.noreply.github.com>
    Co-authored-by: ananya-kollengreth <ananya-kollengreth@users.noreply.github.com>

---
+++ Instructions/1.5-2DPractice.py
@@ -0,0 +1,35 @@
+# Accessing 2D list elements
+a = [[2, 4, 6, 8], [10, 12, 14, 16]]
+# get the following values:
+# 2
+
+# 4
+
+# 8
+
+# 16
+
+
+#Modifying a 2D List's elements 
+b = [[9, 8, 7], 
+     [6, 5, 4], 
+     [3, 2, 1]]
+b1 = [[30, 8, 7], 
+      [6, 5, 4], 
+      [3, 2, 1]]
+#Change the 9 to 30 in b to get b1 
+
+
+# Challenges
+b = [[9, 8, 7], 
+     [6, 5, 4], 
+     [3, 2, 1]]
+b1 = [[9, 100, 7], 
+     [100, 5, 100], 
+     [3, 100, 1]]
+
+# change all of the even numbers to 100
+for i in range(len(b)):
+    for j in range(len(b[0])):
+        pass 
+        #insert code here      
